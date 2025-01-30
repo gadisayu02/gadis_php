e883e72f862a93eb2bdb5dd1c8223572b02170f9
gadisayu02 <gadisayu914@gmail.com>
2025-01-30 13:00:20 +0700

    rawr

--- form/index.php
+++ form/index.php
@@ -3,15 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>form biodata</title>
     <body>
         <form action="form4.html" method="post">
