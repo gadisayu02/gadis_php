d904ed6693ee8b0192e1343fdf97b5ececf711e5
gadisayu02 <gadisayu914@gmail.com>
2025-02-13 12:46:00 +0700

    ke2

---
+++ form/proses.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>peoses.php</title>
+    <head>
+</head>
+<body>
+    <?php
+    if(issert($_POST['ppdbsiswa']))
+    ?>
+
+    <table border=1 style="border-collapse : collapse: widht : 50%:">
+        <tr>
+            <td>NISN</td>
+            <td>:</td>
+            <td>
+                <?=$_POST['NISN'] ?>
+</td>
+</tr>
+<td>nama</td>
+<td>:</td>
+<td>
+    <?=$_POST['NAMA'] ?>
+</td>
+</tr>
+<tr>
+    <td>tempat lahir</td>
+    <td>:</td>
+    <td>
+        <?$_POST['tempatlahir'] ?>
+</td>
+</tr>
+<tr>
+    <td>tanggal lahir</td>
+    <td>:</td>
+    <td>
+        <?$_POST['tanggallahir'] ?>
+</td>
+</tr>
+<tr>
+    <td>gender</td>
+    <td>:</td>
+    <td>
+        <?$_POST['gender'] ?>
+</td>
+</tr>
+<tr>
+
+<td>agama</td>
+<td>:</td>
+<td>
+    <?$_POST['agama'] ?>
+</td>
+</tr>
+<tr>
+
+ <td>alamat</td>
+ <td>:</td>
+ <td>
+    <?$_POST['alamat'] ?>
+</td>
+</tr>
+<tr>
+
+<td>asal sekolah</td>
+<td>:</td>
+<td>
+    <?$_POST['asal sekolah'] ?>
+</td>
+</tr>
+<tr>
+
+<td>pilihan jurusan 1</td>
+<td>:</td>
+<td>
+    <?$_POST['pilihan jurusan 1'] ?>
+</td>
+</tr>
+<tr>
+
+<td>pilihan jurusan 2</td>
+<td>:</td>
+<td>
+    <?$_POST['pilihan jurusan 2'] ?>
+</td>
+</tr>
+<tr>
+
+<td>pilihan jurusan 3</td>
+<td>:</td>
+<td>
+    <?$_POST['pilihan jurusan 3'] ?>
+</td>
+</tr>
+<tr>
+    <td>ekstrakulikuler</td>
+    <td>:</td>
+    <td>
+        <?$_POST['ekstrakulikuler'] ?>
+</td>
+</tr>
+<tr>
+        
+
+
+
+
+
+
+        
+
